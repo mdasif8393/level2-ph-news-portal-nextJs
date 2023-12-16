ece1dd45afb842864d8f4be482e8150635bbbffe
unknown <md.asif.8393@gmail.com>
2023-12-16 10:33:45 +0600

    [Done --> get all news and show]

---
+++ src/pages/news/[newsId].js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NewsDetailPage = () => {
+    return (
+        <div>
+            <h1>This is news detail page</h1>
+        </div>
+    );
+};
+
+export default NewsDetailPage;
