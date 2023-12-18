e15a85c8b5ae299d896df04ebcd0c4c46965bada
unknown <md.asif.8393@gmail.com>
2023-12-18 22:55:11 +0600

    [Done --> Create our server using mongodb]

--- src/pages/index.js
+++ src/pages/index.js
@@ -29,7 +29,7 @@ const HomePage = ({ allNews }) => {
       </Head>
       <BannerHeader />
       {/* <AllNews allNews={allNews} /> */}
-      <AllNews allNews={data} />
+      <AllNews allNews={allNews} />
     </>
   );
 };
@@ -40,11 +40,12 @@ HomePage.getLayout = function getLayout(page) {
 };
 
 export const getServerSideProps = async () => {
-  const res = await fetch("http://localhost:5000/news");
+  const res = await fetch("http://localhost:3000/api/news");
   const data = await res.json();
   return {
     props: {
-      allNews: data
+      allNews: data.data
     },
+    // revalidate : 10,
   }
 }
