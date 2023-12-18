893e7ebd0e789a408c06c9befc0db66d78dcf60b
unknown <md.asif.8393@gmail.com>
2023-12-18 21:55:39 +0600

    [Done --> Add Redux and Lazy Loading]

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,21 @@
 import Head from "next/head";
 import RootLayout from "@/components/Layouts/RootLayout";
-import Banner from "@/components/UI/Banner";
+// import Banner from "@/components/UI/Banner";
 import AllNews from "@/components/UI/AllNews";
+import { useGetNewsesQuery } from "@/redux/api/api";
+import dynamic from 'next/dynamic'
 
 
 const HomePage = ({ allNews }) => {
+
+  const { data, isLoading, isError, error } = useGetNewsesQuery();
+  console.log(data);
+
+  const BannerHeader = dynamic(() => import('@/components/UI/Banner'), {
+    ssr: false,
+    loading: () => <h1>Loading...</h1>,
+  })
+
   return (
     <>
       <Head>
@@ -16,8 +27,9 @@ const HomePage = ({ allNews }) => {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Banner />
-      <AllNews allNews={allNews} />
+      <BannerHeader />
+      {/* <AllNews allNews={allNews} /> */}
+      <AllNews allNews={data} />
     </>
   );
 };
@@ -27,13 +39,12 @@ HomePage.getLayout = function getLayout(page) {
   return <RootLayout>{page}</RootLayout>;
 };
 
-export const getStaticProps = async () => {
+export const getServerSideProps = async () => {
   const res = await fetch("http://localhost:5000/news");
   const data = await res.json();
   return {
     props: {
       allNews: data
     },
-    revalidate: 10,
   }
 }
